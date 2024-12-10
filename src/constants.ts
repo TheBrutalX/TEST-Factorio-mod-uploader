b8e5395a0b842f03f889dbfcc6a543af64092ad3
Gianmarco Venturi <gmv1997@gmail.com>
2024-12-10 12:43:36 +0100

    feat: update constants for input variables to enhance clarity and organization in GitHub Actions
    #23

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
-// #region Environment Variables
-export const MOD_FOLDER = 'MOD-FOLDER';
+// #region Environment Input from GitHub Actions
+export const INPUT_MOD_FOLDER = 'MOD-FOLDER';
 export const INPUT_MOD_NAME = 'MOD-NAME';
+export const INPUT_MOD_VERSION = 'MOD-VERSION';
+export const INPUT_DOTIGNORE_FILE = 'DOTIGNORE-FILE';
 export const INPUT_ZIP_FILE = 'ZIP-FILE';
 export const INPUT_FACTORIO_API_KEY = 'FACTORIO-API-KEY';
+//#region Environment Input from GitHub Actions
